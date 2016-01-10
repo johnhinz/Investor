64c5d940f6aa4e6c5a20cad042c12319013fd450
jhinz <jhinz402@hotmail.com>
2016-01-10 14:39:33 -0500

    Add client logic class

---
+++ Investor/Investor.Common.Service.Client.Logic/ClientLogic.cs
@@ -0,0 +1,25 @@
+﻿using Investor.Common.Service.Client.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Investor.Common.Service.Client.Poco;
+
+namespace Investor.Common.Service.Client.Logic
+{
+    public class ClientLogic : IClientLogic
+    {
+        private IClientRepository _repository;
+
+        public ClientLogic(IClientRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ClientPoco Get(long id)
+        {
+            return _repository.Get(id);
+        }
+    }
+}
