a14e9cd83e7619920b277a4086769d2034c873dd
John Hinz <jhinz402@hotmail.com>
2016-01-16 23:09:02 -0500

    Added test projects and setup initial 'Get' test

---
+++ Investor/Investor.Common.Service.Client.Test/Stubs/LogicStub.cs
@@ -0,0 +1,18 @@
+﻿using Investor.Common.Service.Client.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Investor.Common.Service.Client.Poco;
+
+namespace Investor.Common.Service.Client.Test.Stubs
+{
+    class LogicStub : IClientLogic
+    {
+        public ClientPoco Get(long id)
+        {
+            return new ClientPoco() { FirstName = "Joe", LastName = "Smith" };
+        }
+    }
+}
