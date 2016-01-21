fc050de07a314c3c53d362467f7010d84cd4a270
khyatinaik <khyati3680@rediffmail.com>
2016-01-21 11:39:05 -0500

    Company poco class created

--- Investor/Investor.Common.Service.Company.Poco/CompanyPoco.cs
+++ Investor/Investor.Common.Service.Company.Poco/CompanyPoco.cs
@@ -9,6 +9,14 @@ namespace Investor.Common.Service.Company.Poco
     public class CompanyPoco
     {
         public string CompanyName { get; set; }
-        
+        public string ContactName { get; set; }
+
+        public long AddressId{get; set;}
+        public long PhoneNumber { get; set; }
+        public long AgentID { get; set; }
+
+
+
+
     }
 }
