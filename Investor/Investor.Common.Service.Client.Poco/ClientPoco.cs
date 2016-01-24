f084e08f5fb21c2f0abb8930dc44d00c3793b128
John Hinz <jhinz402@hotmail.com>
2016-01-24 00:40:30 -0500

    Intial crack at adding an address to a client

--- Investor/Investor.Common.Service.Client.Poco/ClientPoco.cs
+++ Investor/Investor.Common.Service.Client.Poco/ClientPoco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Investor.Common.Service.Address.Poco;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,11 @@ namespace Investor.Common.Service.Client.Poco
     public class ClientPoco
     {
         public long Id { get; set; }
-        public long AddressId { get; set; }
+        //public long AddressId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DoB { get; set; }
         public string SocialIns { get; set; }
+        public virtual AddressPoco Address { get; set; }
     }
 }
