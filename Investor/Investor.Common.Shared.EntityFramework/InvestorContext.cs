8adf455c3d5e4d4483b22702c4cda4e79133cb30
John Hinz <jhinz402@hotmail.com>
2016-01-24 00:41:00 -0500

    Inital crack at adding client address relation

--- Investor/Investor.Common.Shared.EntityFramework/InvestorContext.cs
+++ Investor/Investor.Common.Shared.EntityFramework/InvestorContext.cs
@@ -1,4 +1,5 @@
-﻿using Investor.Common.Service.Client.Poco;
+﻿using Investor.Common.Service.Address.Poco;
+using Investor.Common.Service.Client.Poco;
 using Investor.Common.Service.Investment.Poco;
 using Investor.Common.Shared.EntityFramework.Mapping;
 using System;
@@ -21,11 +22,20 @@ namespace Investor.Common.Shared.EntityFramework
         {
             modelBuilder.Configurations.Add(new ClientMapping());
             modelBuilder.Configurations.Add(new InvestmentMapping());
+            modelBuilder.Configurations.Add(new AddressMapping());
+
+            // specify client to address relation
+            modelBuilder.Entity<ClientPoco>()
+                .HasOptional<AddressPoco>(c => c.Address);
+                //.WithOptionalDependent(a => a.Id)
+                //.WithOptional(a => a.Id)
+                //.HasForeignKey(c => c.AddressId)
         }
 
         public DbSet<ClientPoco> Clients { get; set; }
 
         public DbSet<InvestmentPoco> Investments { get; set; }
 
+        public DbSet<AddressPoco> Addresses { get; set; }
     }
 }
---
+++ Investor/Investor.Common.Shared.EntityFramework/Mapping/AddressMapping.cs
@@ -0,0 +1,19 @@
+﻿using Investor.Common.Service.Address.Poco;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Common.Shared.EntityFramework.Mapping
+{
+    class AddressMapping : EntityTypeConfiguration<AddressPoco>
+    {
+        public AddressMapping()
+        {
+            ToTable("Address");
+            HasKey(a => a.Id);
+        }
+    }
+}
