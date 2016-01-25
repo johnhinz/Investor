e3ee9385d3c0553b0fbe4d59a2b5f10e9b5ee492
jhinz <jhinz402@hotmail.com>
2016-01-25 16:22:51 -0500

    Migration update

---
+++ Investor/Investor.Common.Shared.EntityFramework/Migrations/201601252122275_githubUpdate.cs
@@ -0,0 +1,16 @@
+namespace Investor.Common.Shared.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class githubUpdate : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
