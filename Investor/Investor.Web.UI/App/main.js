6b37cc2e1d00d7f12590cd32b351dee44bfed284
John Hinz <jhinz402@hotmail.com>
2016-01-31 08:43:23 -0500

    added simple web project

---
+++ Investor/Investor.Web.UI/App/main.js
@@ -0,0 +1,36 @@
+﻿var app;
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+    }
+})();
+
+app.controller('ClientController', function ($scope, ClientService) {
+    getClients();
+    function getClients() {
+        ClientService.getClients()
+            .success(function (clients) {
+                $scope.clients = clients;
+            })
+            .error(function (error) {
+                $scope.status = 'Unable to load client data: ' + error.message;
+            });
+    }
+});
+
+
+app.factory('ClientService', ['$http', function ($http) {
+    var urlBase = 'http://localhost:13254/api.invest.com/clients';
+    var ClientService = {};
+    ClientService.getClients = function () {
+        return $http.get(urlBase + '/3');
+    };
+    return ClientService;
+}]);
