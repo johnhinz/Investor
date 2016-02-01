6bbb59955138f6e70679fbaf41fd293bb77f7eac
Kinjal Punjabi <kinjalahuja10@gmail.com>
2016-02-01 12:04:34 -0500

    Removed extra files from the Company Structure and updated AddressPoco.

--- Investor/Investor.Common.Service.Address.Poco/AddressPoco.cs
+++ Investor/Investor.Common.Service.Address.Poco/AddressPoco.cs
@@ -11,5 +11,9 @@ namespace Investor.Common.Service.Address.Poco
         public long Address_Id { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
+
+        public string Province { get; set; }
+
+        public string Postal_Code { get; set; }
     }
 }
