ee0628a101cc85c8db56b51d76d3fee04c2c3732
khyatinaik <khyati3680@rediffmail.com>
2016-03-11 17:12:02 -0500

    Created PhoneNumberPcoco class

---
+++ Investor/Investor.Common.Shared.Pocos/PohoneNumberPoco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Common.Shared.Pocos
+{
+   public class PohoneNumberPoco
+    {
+        public long Id { get; set; }
+        public string PhoneNo { get; set; }
+        public string type { get; set; }
+    }
+}
