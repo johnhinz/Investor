0f6e08bb396d061d60053930533d75a5ae5afa6b
John Hinz <jhinz402@hotmail.com>
2016-03-18 10:56:13 -0400

    Added clientaddressmapping and clientaddresspoco classes

---
+++ Investor/Investor.Common.Shared.EntityFramework/Mapping/ClientAddressMapping.cs
@@ -0,0 +1,20 @@
+﻿using Investor.Common.Shared.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Common.Shared.EntityFramework.Mapping
+{
+    class ClientAddressMapping : EntityTypeConfiguration<ClientAddressPoco>
+    {
+        public ClientAddressMapping()
+        {
+            ToTable("Address");
+            HasKey(a => a.Id);
+        }
+
+    }
+}
