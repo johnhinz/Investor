ef747d397e261785c55c538f60ad90b5abb3e283
John Hinz <jhinz402@hotmail.com>
2016-04-03 22:17:59 -0400

    Added DataContract and DataMember attributes to Pocos to control serialization.

--- Investor/Investor.Common.Shared.Pocos/InvestmentPoco.cs
+++ Investor/Investor.Common.Shared.Pocos/InvestmentPoco.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Investor.Common.Shared.Pocos
 {
+    [DataContract]
     public class InvestmentPoco
     {
+        [DataMember]
         public long Id { get; set; }
+        [DataMember]
         public string InvestmentType { get; set; }
+        [DataMember]
         public long CompanyId { get; set; }
         public virtual ICollection<ClientPoco> Clients { get; set; }
     }
---
+++ Investor/Investor.Common.Shared.Pocos/LogPoco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Common.Shared.Pocos
+{
+    public class LogPoco
+    {
+        public long Id { get; set; }
+        public DateTime LogDate { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+}
