969414c40d6ec09f9ca4b378d4f702fe0c306187
John Hinz <jhinz402@hotmail.com>
2016-04-18 08:52:50 -0400

    added angular-cookies

---
+++ Investor/Investor.Common.Shared.DataTransferObjects/ClientAddressDto.cs
@@ -0,0 +1,19 @@
+﻿using Investor.Common.Shared.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investor.Common.Shared.DataTransferObjects
+{
+    public class ClientAddressDto
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+        public string SocialIns { get; set; }
+        ICollection<ClientAddressPoco> Addresses {get;set;}
+    }
+}
